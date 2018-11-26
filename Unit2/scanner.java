18cf3cf36449c6e19dd9a98f874687ed3c25b0a3
hk21702 <hk21702@gmail.com>
2018-11-26 11:20:17 -0500

    CC

---
+++ Unit2/scanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class scanner{
+    public static void main(String[] args){
+        Scanner scany = new Scanner(System.in);
+        
+        
+        System.out.println("System Start");
+        
+        double y = scany.nextDouble();
+        double x = Math.pow(2,y);
+
+        System.out.println(x+1);
+        scany.close();
+    }
+}
